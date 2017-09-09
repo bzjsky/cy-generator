01d700fdfd58845c966045b3ce2bb1cc16be90fc
亍、伊 <bzjsky@sina.com>
2017-09-09 09:46:40 +0800

    优化多个实现类，启动spring boot 异常问题

--- templates/template_2/cy-template/${basepackage_dir}/business/${namespace_dir}/mapper/${className}Mapper.java
+++ templates/template_2/cy-template/${basepackage_dir}/business/${namespace_dir}/mapper/${className}Mapper.java
@@ -4,12 +4,17 @@
 package ${basepackage}.business.${namespace}.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+
+import ${basepackage}.business.${namespace}.entity.${className};
+<#if (!table.pkColumn.javaType?contains("lang") && table.pkColumn.javaType?contains("."))>
+import ${javaType};
+</#if>
 import com.cy.sdk.mapper.BasicsMapper;
 
 /**
 <#include "/java_description.include">
 */
 @Mapper
-public interface ${className}Mapper extends BasicsMapper{
+public interface ${className}Mapper extends BasicsMapper<${className}, ${table.pkColumn.simpleJavaType}>{
     
 }
